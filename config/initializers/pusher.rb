872cab38fd3096a78a3be004faaf856ff4bf9f86
Andrew Petaisto <ahpetaisto@gmail.com>
2013-11-08 14:00:55 -0800

    removed pusher.rb from config/initializers

--- config/initializers/pusher.rb
+++
@@ -1,9 +0,0 @@
-require 'pusher'
-
-Pusher.app_id = ENV["PUSHER_APP_ID"]
-Pusher.key = ENV["PUSHER_KEY"]
-Pusher.secret = ENV["PUSHER_SECRET"]
-
-Pusher.url = "http://#{ENV['PUSHER_KEY']}:#{ENV['PUSHER_SECRET']}@api.pusherapp.com/apps/#{ENV['PUSHER_APP_ID']}"
-
-Pusher.logger = Rails.logger
